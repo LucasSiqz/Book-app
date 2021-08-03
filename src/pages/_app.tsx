22e3605863cc06b55deb9ebc18cf2c128c5795de
Lucas Siqueira <lucassiqzro@gmail.com>
2021-08-03 05:52:30 -0300

    Add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NextNprogress from 'nextjs-progressbar'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
@@ -13,10 +14,15 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
         <meta name="description" content="Book app" />
       </Head>
       <GlobalStyles />
+      <NextNprogress
+        color="#FF6978"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   )
